ef1afc283e85b6acc2bd760bf2967353d61b99ed
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2025-05-15 17:32:48 +0200

    feat: implement getLatestPosts function and update index page to display latest posts

--- src/lib/wp.ts
+++ src/lib/wp.ts
@@ -16,3 +16,37 @@ export const getPageInfo = async (slug: string) => {
 
   return { title, content } 
 }
+
+export const getLatestPosts = async ({perPage = 10}: {perPage?: number} = {}) => {
+  const response = await fetch(`${apiUrl}/posts?per_page=${perPage}`)
+  if (!response.ok) throw new Error("Failed to fetch latest posts")
+
+  const results = await response.json()
+  if (!results.length) throw new Error("No posts found")
+
+  const posts = results.map((post: any) => {
+
+    /* 
+    const title = post.title.rendered
+    const excerpt = post.excerpt.rendered
+    const content = post.content.rendered
+    const date = post.date
+    const slug = post.slug
+    // Or we can do this with the last 2
+    const { date, slug } = post
+    */
+
+    // This is the simplified version of the above code
+    const {
+      title: { rendered: title },
+      excerpt: { rendered: excerpt },
+      content: { rendered: content },
+      date,
+      slug
+    } = post
+
+    return {title, excerpt, content, date, slug}
+  })
+
+  return posts 
+}
